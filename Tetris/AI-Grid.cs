1a7e2390309bffa692c94d82d3e256e253874261
Danylovich <55502089+Danylovich@users.noreply.github.com>
2025-01-28 14:02:43 +0200

    added FigureMoveOption class, added CheckAllPositionsProjectedFigures, CheckAllPositionsInDirection, GetCurrentPositionMoveOption methods

--- Tetris/AI-Grid.cs
+++ Tetris/AI-Grid.cs
@@ -8,16 +8,57 @@ namespace Tetris
 {
     public class AI_Grid : GameMain
     {
-        public AI_Grid(int rows, int cols, Figure bufferFigure, Figure currentFigure, List<List<GridValue>> grid) : base(rows, cols)
+        public List<FigureMoveOption> FigureMoveOptions { get; private set; }
+            = new List<FigureMoveOption>();
+        public AI_Grid(int rows, int cols, Figure bufferFigure, Figure currentFigure, Figure projectedFigure, List<List<GridValue>> grid) : base(rows, cols)
         {
             BufferFigure = setFigureClone(bufferFigure);
             CurrentFigure = setFigureClone(currentFigure);
+            ProjectedFigure = setFigureClone(projectedFigure);
             Grid = new List<List<GridValue>>(grid);
         }
 
         public Figure setFigureClone(Figure cloneFigure)
         {
             return cloneFigure.Clone();
+        }
+
+        public void CheckAllPositionsProjectedFigures()
+        {
+            FigureMoveOptions.Add(GetCurrentPositionMoveOption());
+
+            int[] currentColumnIndexOnGrid = new int[CurrentFigure.ColumnCount];
+            Array.Copy(CurrentFigure.ColumnIndexOnGrid, currentColumnIndexOnGrid, CurrentFigure.ColumnCount);
+
+            CheckAllPositionsInDirection(MoveLeft);
+
+            CurrentFigure.ColumnIndexOnGrid = currentColumnIndexOnGrid;
+
+            CheckAllPositionsInDirection(MoveRight);
+        }
+
+        private void CheckAllPositionsInDirection(Func<bool> moveDirection)
+        {
+            bool canMove = true;
+            while (true)
+            {
+                canMove = moveDirection();
+                if (canMove)
+                    FigureMoveOptions.Add(GetCurrentPositionMoveOption());
+                else
+                    break;
+            }
+        }
+
+        private FigureMoveOption GetCurrentPositionMoveOption()
+        {
+            return new FigureMoveOption(ProjectedFigure,
+                CurrentFigure.ColumnIndexOnGrid[0], CalculateMoveOption(ProjectedFigure));
+        }
+
+        public int CalculateMoveOption(Figure projectedFigure)
+        {
+
         }
     }
 }
---
+++ Tetris/FigureMoveOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class FigureMoveOption
+    {
+        public int Score { get; private set; }
+        public int PositionOfCurrentFigure { get; private set; } // ліва верхня клітинка поля фігури на полі гри
+        public Figure ProjectedFigure { get; private set; }
+        public FigureMoveOption(Figure projectedFigure, int positionOfCurrentFigure, int score)
+        {
+            ProjectedFigure = projectedFigure;
+            PositionOfCurrentFigure = positionOfCurrentFigure;
+            Score = score;
+        }
+    }
+}
